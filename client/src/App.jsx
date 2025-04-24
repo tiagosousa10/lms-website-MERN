2c78cab4de4876bcb270bd3fea7e51247ad8208b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-24 16:14:14 +0100

    navbar updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home'
 import CoursesList from './pages/student/CoursesList'
 import CourseDetails from './pages/student/CourseDetails'
@@ -15,9 +15,14 @@ import Navbar from './components/student/Navbar'
 
 
 const App = () => {
+
+  const isEducatorRoute = useMatch("/educator/*")
+
   return (
     <div className='text-default min-h-screen bg-white'>
-      <Navbar />
+      {!isEducatorRoute && (
+        <Navbar />
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />

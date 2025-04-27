a8853e281e2a1b56ab2277acb7bc55c1ddb42bf0
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-27 17:08:23 +0100

    taiilwindcss config updated with new fontsizes

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../../components/student/Hero'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero />
     </div>
   )

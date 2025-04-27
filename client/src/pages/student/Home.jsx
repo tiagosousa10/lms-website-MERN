d30f3cc5bfe609f0e6a80eac9f7f5fb97bf777be
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-27 17:04:47 +0100

    Hero component updated

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../../components/student/Hero'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
 

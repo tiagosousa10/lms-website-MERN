10ea03d23f899474e633ae1ce6fa38a853924c1c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-27 17:48:09 +0100

    Fetching all courses and map them to show

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,26 @@
-import {createContext} from "react";
+import {createContext, useEffect, useState} from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
+   const currency = import.meta.env.VITE_CURRENCY
+
+   const [allCourses,setAllCourses] = useState([])
+
+   //Fetch all courses
+   const fetchAllCourses = async () => {
+      setAllCourses(dummyCourses)
+   }
+
+   useEffect(() => {
+      fetchAllCourses()
+   },[])
+
    const value = {
-      
+      currency,
+      allCourses
    }
 
    return (

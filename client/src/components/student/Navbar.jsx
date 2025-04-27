948a7ed6bae60e669e4322e0df4423895d92d492
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-27 16:56:52 +0100

    navbar updated with clerk functionalities

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -16,20 +16,37 @@ const Navbar = () => {
       <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
-          <button>Become Educator</button>
+          { user && 
+          <>
+            <button>Become Educator</button>
           | <Link to={"/my-enrollments"} >My Enrollments</Link>
+          </>}
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+       { user ? <UserButton /> 
+              : 
+        <button 
+          onClick={() => openSignIn()}
+          className='bg-blue-600 text-white px-5 py-2 rounded-full'>
+            Create Account
+        </button>}
       </div>
 
       {/*Mobile Screens */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500 '>
-        <div>
-        <button>Become Educator</button>
-        | <Link to={"/my-enrollments"} >My Enrollments</Link>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+        { user && 
+          <>
+            <button>Become Educator</button>
+          | <Link to={"/my-enrollments"} >My Enrollments</Link>
+          </>
+        }
         </div>
+        {
+          user ? <UserButton /> 
+              : 
+              <button onClick={() => openSignIn()}><img src={assets.user_icon} alt="user_icon" /></button>
+        }
 
-        <button><img src={assets.user_icon} alt="user_icon" /></button>
 
       </div>
     </div>

09c5946112bb45675bce930072336da08230a77c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-29 11:55:37 +0100

    updating the Context and navbar

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -12,7 +12,7 @@ const CoursesSection = () => {
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br/> business and wellness, our courses are crafted to deliver results.</p>
 
-      <div className='grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
+      <div className='grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4'>
         {allCourses.slice(0,4).map((course, index) => (
           <CourseCard key={index} course={course} />
         ))}

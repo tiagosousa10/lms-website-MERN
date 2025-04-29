b896e4bfe45c07c4a72da8b27b4770d75f851262
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-29 11:50:54 +0100

    course card and section styled

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -1,31 +1,33 @@
 import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { AppContext } from '../../context/AppContext'
+import { Link } from 'react-router-dom'
 
 const CourseCard = ({course}) => {
   const {currency} = useContext(AppContext)
 
   return (
-    <div>
-      <img src={course.courseThumbnail} alt="" />
-      <div>
-        <h3>{course.courseTitle}</h3>
-        <p>{course.educator.name}</p>
+    <Link to={'/course/' + course._id} onClick={() => scrollTo(0,0)} 
+      className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'  >
+      <img className="w-full" src={course.courseThumbnail} alt="" />
+      <div className="p-3 text-left">
+        <h3 className="text-base font-semibold">{course.courseTitle}</h3>
+        <p className="text-gray-500">{course.educator.name}</p>
 
-        <div>
+        <div className="flex items-center space-x-2">
           <p>4.5</p>
-          <div>
+          <div className="flex">
             {[...Array(5)].map((_, index) => (
-              <img key={index} src={assets.star} alt="" />
+              <img className='w-3.5 h-3.5' key={index} src={assets.star} alt="" />
             ))}
           </div>
 
-          <p>22</p>
+          <p className='text-gray-500'>22</p>
         </div>
 
-        <p>{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
+        <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -10,9 +10,9 @@ const CoursesSection = () => {
   return (
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br/> business and wellness, our courses are crafted to deliver results.</p>
 
-      <div>
+      <div className='grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
         {allCourses.slice(0,4).map((course, index) => (
           <CourseCard key={index} course={course} />
         ))}

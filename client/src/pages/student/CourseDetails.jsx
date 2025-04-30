b5c25cbfca225e40539350e9f0110d6c82b02c19
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-30 19:34:14 +0100

    course Details jsx updated

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
+import { assets } from '../../assets/assets'
 
 const CourseDetails = () => {
   const {id} = useParams()
 
   const [courseData,setCourseData] = useState(null)
 
-  const {allCourses} = useContext(AppContext)
+  const {allCourses, calculateRating} = useContext(AppContext)
 
   const fetchCourseData = async () => {
-    const findCourse = allCourses.find((course) => course.id === id)
+    const findCourse = allCourses.find((course) => course._id === id)
     setCourseData(findCourse)
   }
 
@@ -18,18 +20,41 @@ const CourseDetails = () => {
     fetchCourseData()
   }, [])
 
-  return (
+  return courseData ?  (
+    <>
+    
     <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
       <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
 
       {/* //left column */}
-      <div></div>
+      <div className='max-w-xl z-10 text-gray-500'>
+        <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData?.courseTitle}</h1>
+        <p 
+          className='pt-4 md:text-base text-sm'
+          dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0,200)}}></p>
+
+          {/* review and ratings */}
+
+           <div className="flex items-center space-x-2">
+              <p>{calculateRating(courseData)}</p>
+              <div className="flex">
+                {[...Array(5)].map((_, index) => (
+                  <img className='w-3.5 h-3.5' key={index} src={index < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt="" />
+                ))}
+              </div>
+    
+              <p className='text-blue-600'>({courseData.courseRatings.length} {courseData.length > 1 ? 'ratings' : 'rating'})</p>
+
+              <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student'} </p>
+            </div>
+      </div>
 
       {/* //right column */}
       <div></div>
 
     </div>
-  )
+    </>
+  ) : <Loading />
 }
 
 export default CourseDetails

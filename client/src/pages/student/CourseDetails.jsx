102b3a53b7c2cbc09633e5ce06fa1361e8af191e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-01 16:13:32 +0100

    right side of course details jsx updated

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -10,6 +10,7 @@ const CourseDetails = () => {
 
   const [courseData,setCourseData] = useState(null)
   const [openSections, setOpenSections] = useState({})
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
 
   const {
     allCourses,
@@ -39,7 +40,7 @@ const CourseDetails = () => {
   return courseData ?  (
     <>
     
-    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-20 pt-10 text-left">
       <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
 
       {/* //left column */}
@@ -114,7 +115,7 @@ const CourseDetails = () => {
       </div>
 
       {/* //right column */}
-      <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px] '>
+      <div className="max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
         <img src={courseData.courseThumbnail} alt="" />
         <div className='p-5'>
           <div className='flex items-center gap-2'>
@@ -122,14 +123,54 @@ const CourseDetails = () => {
             <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
           </div>
 
-          <div>
-            <p></p>
+          <div className='flex gap-3 items-center pt-2'>
+            <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>{currency} {(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
+            <p className='md:text-lg text-gray-500 line-through '>{currency} {courseData.coursePrice.toFixed(2)}</p>
+            <p className='md:text-lg text-gray-500'>{courseData.discount}% off</p>
+          </div>
+
+          <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
+            <div className='flex items-center gap-1'>
+              <img src={assets.star} alt="star icon" />
+              <p>{calculateRating(courseData)}</p>
+            </div>
+
+            <div className='h-4 w-px bg-gray-500/400'></div>
+
+              <div className='flex items-center gap-1'>
+                <img src={assets.time_clock_icon} alt="clock icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className='h-4 w-px bg-gray-500/400'></div>
+
+              <div className='flex items-center gap-1'>
+                <img src={assets.lesson_icon} alt="lesson icon" />
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+
+            </div>
+
+            <button className='md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium '>
+              {isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}
+            </button>
+
+            <div className='pt-6'>
+              <p className='md:text-xl text-lg font-medium text-gray-800'>What's in the course?</p>
+              <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>
+                <li>Lifetime access with free updates</li>
+                <li>Step-by-step, hands-on project guidance.</li>
+                <li>Downloadable resources and source code.</li>
+                <li>Quizes to test your knowledge</li>
+                <li>Certificate of completion</li>
+              </ul>
+            </div>
+
           </div>
 
         </div>
       </div>
 
-    </div>
     </>
   ) : <Loading />
 }

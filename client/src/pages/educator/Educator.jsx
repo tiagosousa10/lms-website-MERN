2db5668553e0be7c6dd82f5e8d31143d6c46ed83
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-02 15:51:21 +0100

    educator sidebar first steps

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator Page</h1>
-      {<Outlet />}  
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar />
+      <div className='flex'>
+        <Sidebar />
+        <div className='flex-1'>
+          {<Outlet />}  
+        </div>
+      </div>
     </div>
   )
 }

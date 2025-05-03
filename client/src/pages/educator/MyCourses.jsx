342dcf801f2a3a66634bb4174061ff659552eead
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-03 11:01:51 +0100

    my courses section table body updated

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -31,6 +31,23 @@ const MyCourses = () => {
                 <th className='px-4 py-3 font-semibold truncate'>All Courses</th>
               </tr>
             </thead>
+            <tbody className='text-sm text-gray-500'>
+            {courses.map((course) => (
+              <tr key={course._id} className='border-b border-gray-500/20 '>
+                <td className='md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate'>
+                  <img src={course.courseThumbnail} alt="course image" className='w-16' />
+                  <span className='truncate hidden md:block'>{course.courseTitle}</span>
+                </td>
+
+                <td className='px-4 py-3'>
+                  {currency} {Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.coursePrice) / 100)}
+                </td>
+                <td className='px-4 py-3'>{course.enrolledStudents.length}</td>
+                <td className='px-4 py-3'>{new Date(course.createdAt).toLocaleDateString()}</td>
+              </tr>
+            ))}
+            </tbody>
+
           </table>
         </div>
       </div>

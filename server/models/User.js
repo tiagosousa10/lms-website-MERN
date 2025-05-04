a8711264731565cf452b71744fefac95a1c4f1ec
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-04 12:04:09 +0100

    create User Model

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {type:String, required: true},
+    name: {type:String, required: true},
+    email: {type:String, required: true},
+    imageUrl: {type:String, required: true},
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+      }
+    ],
+  },
+  { // Add this line to enable timestamps -> createdAt and updatedAt
+    timestamps: true,
+  }
+)
+
+
+const User = mongoose.model('User', userSchema);
+export default User;

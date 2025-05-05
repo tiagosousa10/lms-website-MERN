45f79fcda67482ff41bc2c963e0959b0f71b1d4a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-05 11:20:06 +0100

    create the route for the educator

---
+++ server/routes/educatorRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { updateRoleToEducator } from '../controllers/educatorController.js';
+
+const educatorRouter = express.Router();
+
+//add educator role
+educatorRouter.get('/update-role', updateRoleToEducator)
+
+
+export default educatorRouter;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
 
 //Initialize express app
 const app = express();
@@ -12,12 +14,14 @@ await connectDB();
 
 //Middlewares
 app.use(cors());
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/', (req, res) => {
   res.send("API is running");
 })
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
 
 //Port
 const PORT = process.env.PORT || 5000;

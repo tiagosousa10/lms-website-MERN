5324e80bb6a954dda04eb5cfd80568af37566b4e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-05 11:42:13 +0100

    create cloudinary config

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,7 +1,7 @@
 import {clerkClient} from "@clerk/express";
 
 //update role to educator
-export const updateRoleToEducator = async () => {
+export const updateRoleToEducator = async (req,res) => {
   try {
     const userId = req.auth.userId;
 

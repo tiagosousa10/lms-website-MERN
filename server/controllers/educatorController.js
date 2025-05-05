0e7ba4c63ef6d22acac3ddb4b8cd7c4dba7b7c36
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-05 12:01:35 +0100

    create addCourse functionality, and multer config added

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,6 @@
 import {clerkClient} from "@clerk/express";
+import Course from "../models/Course";
+import {v2 as cloudinary} from "cloudinary";
 
 //update role to educator
 export const updateRoleToEducator = async (req,res) => {
@@ -16,5 +18,33 @@ export const updateRoleToEducator = async (req,res) => {
   } catch(error) {
     res.json({success: false, message: error.message})
     console.log("Error in updateRoleToEducator", error.message)
+  }
+}
+
+//add new course
+export const addCourse = async (req,res) => {
+  try {
+    const {courseData} = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
+
+    if(!imageFile) {
+      return res.json({success: false, message: "Please upload an image"})
+    }
+
+    const parsedCourseData = await JSON.parse(courseData);//parse courseData to json object
+    parsedCourseData.educator = educatorId;
+
+    const newCourse = await Course.create(parsedCourseData) 
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+    newCourse.courseThumbnail = imageUpload.secure_url
+
+    await newCourse.save()
+
+    res.json({success: true, message: "Course added successfully"})    
+
+  } catch(error) {
+    res.json({success: false, message: error.message})
+    console.log("Error in addCourse", error.message)
   }
 }

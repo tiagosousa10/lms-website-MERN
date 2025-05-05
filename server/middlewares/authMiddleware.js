cce53f126a93e2b66035c1ef4bebc8ba05dce0b3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-05 12:06:08 +0100

    protect educator routes middleware added

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import { clerkClient } from "@clerk/express";
+
+//Middleware ( Protect Educator Routes)
+export const protectEducator = async (req, res, next) => {
+  try {
+    const userId = req.auth.userId;
+    const response =  await clerkClient.users.getUser(userId) //get user from clerk
+
+    if(response.publicMetadata.role !== 'educator') {
+      return res.json({success: false, message: "You are not an educator"})
+    }
+
+    next();
+
+  } catch(error) {
+    res.json({success: false, message: error.message})
+    console.log("Error in protectEducator", error.message)
+  }
+}

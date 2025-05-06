3e70cde2418f97ab4229848a16437834f8d3dacd
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-06 19:53:39 +0100

    purchase a course with stripe

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -38,3 +38,6 @@ export const getCourseId = async(req,res) => {
     console.log("Error in getCourseId", error.message)
   }
 }
+
+
+
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
-import User from "../models/User";
+import Stripe from "stripe";
+import Course from "../models/Course.js";
+import User from "../models/User.js";
+import { Purchase } from "../models/Purchase.js";
 
 //get user data
 export const getUserData = async(req,res) => {
@@ -12,7 +15,6 @@ export const getUserData = async(req,res) => {
 
     res.json({success:true, user})
     
-
   } catch(error) {
     res.json({success: false, message: error.message})
     console.log("Error in getUserData", error.message)
@@ -21,16 +23,77 @@ export const getUserData = async(req,res) => {
 
 
 //user enrolled courses with lecture links
-export const userEnrolledCourses = async(req,res) => {
+export const userEnrolledCourses = async (req, res) => {
+
   try {
 
-    const userId = req.auth.userId;
-    const userData = await User.findById(userId).populate('enrolledCourses') // populate enrolledCourses list with course details
+      const userId = req.auth.userId
 
-    res.json({success:true, enrolledCourse: userData.enrolledCourses})
+      const userData = await User.findById(userId)
+          .populate('enrolledCourses')
+
+      res.json({ success: true, enrolledCourses: userData.enrolledCourses })
+
+  } catch (error) {
+      res.json({ success: false, message: error.message })
+      console.log("Error in userEnrolledCourses", error.message)
+  }
+
+}
+
+//purchase course
+export const purchaseCourse = async(req,res) => {
+  try {
+    const {courseId} = req.body;
+    const {origin}= req.headers;
+    const userId = req.auth.userId;
+    const userData = await User.findById(userId)
+    const courseData = await Course.findById(courseId)
+
+    if(!userData || !courseData) {
+      return res.json({success: false, message: "User or course not found"})
+    }
+
+    const purchaseData = {
+      courseId: courseData._id,
+      userId,
+      amount: (courseData.coursePrice / 100).toFixed(2),
+    }
+
+    const newPurchase = await Purchase.create(purchaseData)
+
+    // stripe gateway initialize
+    const stripeInstance= new Stripe(process.env.STRIPE_SECRET_KEY)
+
+    const currency = process.env.CURRENCY.toLowerCase()
+
+    //creating line items for stripe
+    const line_items=[{
+      price_data: {
+        currency,
+        product_data: {
+          name: courseData.courseTitle,
+        },
+        unit_amount: Math.floor(newPurchase.amount) * 100
+      },
+      quantity: 1
+    }]
+
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-enrollments`,
+      cancel_url: `${origin}/`,
+      line_items: line_items,
+      mode: 'payment',
+      metadata: {
+        purchaseId: newPurchase._id.toString(),
+      }
+    })
+
+    res.json({success: true, sessionUrl: session.url})
+
 
   } catch(error) {
     res.json({success: false, message: error.message})
-    console.log("Error in userEnrolledCourses", error.message)
+    console.log("Error in purchaseCourse", error.message)
   }
 }

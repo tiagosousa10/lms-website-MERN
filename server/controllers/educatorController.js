5415a2fbfcc206f3c2ac0c32d62651103c176063
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-06 19:02:50 +0100

    get educator dashboard data

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,6 +1,7 @@
 import {clerkClient} from "@clerk/express";
 import Course from "../models/Course.js";
 import {v2 as cloudinary} from "cloudinary";
+import { Purchase } from "../models/Purchase.js";
 
 //update role to educator
 export const updateRoleToEducator = async (req,res) => {
@@ -59,5 +60,55 @@ export const getEducatorCourses = async(req,res) => {
   } catch(error) {
     res.json({success: false, message: error.message})
     console.log("Error in getEducatorCourses", error.message)
+  }
+}
+
+// get educator dashboard data (total earning, enrolled students, number of courses)
+
+export const educatorDashboardData = async () => {
+  try {
+    const educator = req.auth.userId;
+    const courses = await Course.find({educator})
+
+    const totalCourses = courses.length; //total number of courses
+    const courseIds = courses.map((course) => course._id) //array of course ids
+
+    //calculate total earnings from purchases
+    const purchases = await Purchase.find({
+      courseId: {
+        $in: courseIds
+      },
+      status: "completed"
+    })
+
+    const totalEarnings = purchases.reduce((sum, purchase ) => sum + purchase.amount, 0);
+
+    //colect unique enrolled students Ids with their course titles
+    const enrolledStudentsData = [];
+    for(const course of courses){
+      const students = await User.find({
+        _id: {
+          $in: course.enrolledStudents
+        }, 
+      },'name imageUrl') //get name and image url of enrolled students
+
+      students.forEach((student) => {
+        enrolledStudentsData.push({
+          courseTitle: course.courseTitle,
+          student
+        })
+      })
+    }
+
+    res.json({success: true, dashboardData : {
+      totalEarnings,
+      enrolledStudentsData,
+      totalCourses
+    }})
+
+
+  } catch(error) {
+    res.json({success: false, message: error.message})
+    console.log("Error in educatorDashboardData", error.message)
   }
 }

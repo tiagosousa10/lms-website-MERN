65c57cff3650e24e1b8f21ec0bb1e5797d87f285
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-06 19:20:27 +0100

    create course routes to get data

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
 
 //Initialize express app
 const app = express();
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 })
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
 
 //Port
 const PORT = process.env.PORT || 5000;

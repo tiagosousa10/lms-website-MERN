96b7abb1255039adb75001a61a09b6682b3bf187
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-06 20:16:55 +0100

    updating the route to use stripe

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 //Port
 const PORT = process.env.PORT || 5000;

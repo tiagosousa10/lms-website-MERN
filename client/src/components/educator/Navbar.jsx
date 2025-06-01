f63ddbe021413f4845a443a97c398ac6483158de
tiagosousa <tiagosousa.tams@hotmail.com>
2025-06-01 19:08:51 +0100

    educator navbar added

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,23 +1,56 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { UserButton, useUser } from "@clerk/clerk-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user } = useUser();
+  const navigate = useNavigate();
 
   return (
-    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
-      <Link to={"/"}>
-        <img src={assets.logo} alt="logótipo" className="w-28 lg:w-32" />
-      </Link>
+    <div className="navbar bg-base-100 shadow-sm px-6 md:px-20">
+      {/* INÍCIO NAVBAR - LOGO */}
+      <div className="navbar-start">
+        <Link to={"/"} className="flex items-center gap-3">
+          <div
+            className="flex items-center gap-4 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <div className="avatar">
+              <div className="size-10 rounded-full overflow-hidden">
+                <img
+                  src="logo.jpg"
+                  className="object-cover object-center w-full h-full cursor-pointer"
+                />
+              </div>
+            </div>
+            <span className="font-medium">tsAcademy</span>
+          </div>
+        </Link>
+      </div>
 
-      <div className="flex items-center gap-5 text-gray-500 relative">
-        <p>Olá! {user ? user.fullName : "Formadores"}</p>
+      {/* CENTRO NAVBAR - Nome de utilizador */}
+      <div className="navbar-center hidden md:flex"></div>
+
+      {/* FIM NAVBAR - Autenticação */}
+      <div className="navbar-end">
         {user ? (
-          <UserButton />
+          <div className="flex items-center gap-4">
+            <p className="text-gray-500 text-sm">
+              Olá! {user ? user.fullName : "Formadores"}
+            </p>
+            <UserButton />
+          </div>
         ) : (
-          <img src={assets.profile_img} alt="perfil" className="max-w-8" />
+          <div className="avatar">
+            <div className="w-8 rounded-full overflow-hidden">
+              <img
+                src={assets.profile_img}
+                alt="perfil"
+                className="object-cover object-center w-full h-full"
+              />
+            </div>
+          </div>
         )}
       </div>
     </div>

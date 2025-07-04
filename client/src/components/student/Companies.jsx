32554368322358a55d907b8d29afbe4a91ce8911
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-04 14:03:18 +0100

    companies updated with a slider effect

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -3,22 +3,43 @@ import { assets } from "../../assets/assets";
 
 const Companies = () => {
   return (
-    <div className="pt-16">
-      <p className="text-base text-gray-500">Confiança dos alunos de</p>
-      <div className="flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5">
-        <img
-          src={assets.microsoft_logo}
-          alt="Microsoft"
-          className="w-20 md:w-28"
-        />
-        <img src={assets.walmart_logo} alt="Walmart" className="w-20 md:w-28" />
-        <img
-          src={assets.accenture_logo}
-          alt="Accenture"
-          className="w-20 md:w-28"
-        />
-        <img src={assets.adobe_logo} alt="Adobe" className="w-20 md:w-28" />
-        <img src={assets.paypal_logo} alt="Paypal" className="w-20 md:w-28" />
+    <div className="pt-16 overflow-hidden">
+      <p className=" text-base-content/70 text-center text-3xl mb-10">
+        Confiança dos alunos de
+      </p>
+
+      <div className="relative max-w-7xl mt-6">
+        <div className="flex gap-12 md:gap-20 animate-slide whitespace-nowrap">
+          {[...Array(2)].map((_, i) => (
+            <React.Fragment key={i}>
+              <img
+                src={assets.microsoft_logo}
+                alt="Microsoft"
+                className="w-20 md:w-28 inline-block"
+              />
+              <img
+                src={assets.walmart_logo}
+                alt="Walmart"
+                className="w-20 md:w-28 inline-block"
+              />
+              <img
+                src={assets.accenture_logo}
+                alt="Accenture"
+                className="w-20 md:w-28 inline-block"
+              />
+              <img
+                src={assets.adobe_logo}
+                alt="Adobe"
+                className="w-20 md:w-28 inline-block"
+              />
+              <img
+                src={assets.paypal_logo}
+                alt="Paypal"
+                className="w-20 md:w-28 inline-block"
+              />
+            </React.Fragment>
+          ))}
+        </div>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,28 +2,36 @@ import daisyui from "daisyui";
 
 // tailwind.config.js
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontSize: {
-        'course-details-heading-small': ['26px', '36px'],
-        'course-details-heading-large': ['36px', '44px'],
-        'home-heading-small': ['28px', '34px'],
-        'home-heading-large': ['48px', '56px'],
-        'default': ['15px', '21px'],
+        "course-details-heading-small": ["26px", "36px"],
+        "course-details-heading-large": ["36px", "44px"],
+        "home-heading-small": ["28px", "34px"],
+        "home-heading-large": ["48px", "56px"],
+        default: ["15px", "21px"],
       },
       gridTemplateColumns: {
-        'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+        auto: "repeat(auto-fit, minmax(200px, 1fr))",
       },
       spacing: {
-        'section-height': '500px',
+        "section-height": "500px",
       },
       maxWidth: {
-        'course-card': '424px',
-
+        "course-card": "424px",
       },
       boxShadow: {
-        'custom-card': '0px 4px 15px 2px rgba(0, 0, 0, 0.1)',
+        "custom-card": "0px 4px 15px 2px rgba(0, 0, 0, 0.1)",
+      },
+      animation: {
+        slide: "slide 25s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
       },
     },
   },

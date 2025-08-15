62043321b1f17dca01158d42ca95c93247e899e5
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-15 14:47:31 +0100

    create testimonial

--- server/controllers/testimonialController.js
+++ server/controllers/testimonialController.js
@@ -56,3 +56,36 @@ export const getTestimonial = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+export const createTestimonial = async (req, res) => {
+  try {
+    const userId = req.auth?.userId; // Clerk
+    if (!userId)
+      return res.json({ success: false, message: "Não autenticado" });
+
+    // verify if user exists in database
+    const user = await User.findById(userId);
+    if (!user)
+      return res.json({ success: false, message: "Utilizador não encontrado" });
+
+    const rating = Number(req.body.rating);
+    const text = String(req.body.text || "").trim();
+
+    // verify rating and text length
+    if (!(rating >= 1 && rating <= 5) || text.length < 10) {
+      return res.json({ success: false, message: "Dados inválidos" });
+    }
+
+    //create testimonial
+    const created = await Testimonial.create({
+      user: String(userId),
+      rating,
+      text,
+    });
+
+    return res.status(201).json({ success: true, testimonial: created });
+  } catch (error) {
+    console.log("Error in createTestimonial:", error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
